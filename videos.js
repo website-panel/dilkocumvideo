f0d7fcefb8df343a79b09ae255b73407150fa1d2
Muhammet Ali KARABAY <149870599+website-panel@users.noreply.github.com>
2025-10-30 20:19:04 +0300

    Update videos.js

--- videos.js
+++ videos.js
@@ -15,7 +15,7 @@ const videoListesi = [
     id: 2,
     title: "Ders 2: 30.10.2025,
     description: "",
-    thumbnail: "thumbnails/ders1.png", // BU YOLUN DOĞRU OLDUĞUNDAN EMİN OLUN
+    thumbnail: "thumbnails/ders2.png", // BU YOLUN DOĞRU OLDUĞUNDAN EMİN OLUN
     videoFiles: {
       "1080p": "https://pixeldrain.com/u/iGjRNto3"
     }
@@ -24,12 +24,13 @@ const videoListesi = [
     id: 3,
     title: "Ders 3: 30.10.2025,
     description: "",
-    thumbnail: "thumbnails/ders1.png", // BU YOLUN DOĞRU OLDUĞUNDAN EMİN OLUN
+    thumbnail: "thumbnails/ders3.png", // BU YOLUN DOĞRU OLDUĞUNDAN EMİN OLUN
     videoFiles: {
       "1080p": "https://pixeldrain.com/u/iGjRNto3"
     }
 }
 ];
+
 
 
 
